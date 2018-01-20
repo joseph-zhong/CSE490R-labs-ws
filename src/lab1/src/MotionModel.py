d0095356fb1afd813bb7d82288b222c8a22340a4
Tim Adamson <tda5@uw.edu>
2018-01-19 20:51:19 -0800

    completed odom and began work on kinematic

--- src/lab1/src/MotionModel.py
+++ src/lab1/src/MotionModel.py
@@ -12,7 +12,12 @@ from pprint import pprint
 # Delete this later
 from nav_msgs.msg import Odometry
 
+# Motion Model Hyperparameters.
+ODOM_NOISE_MEAN = 0.0
+ODOM_NOISE_STD = 1e-3
 
+# Car globals.
+CAR_LEN = 0.33
 
 class OdometryMotionModel:
 
@@ -26,15 +31,24 @@ class OdometryMotionModel:
     
   def motion_cb(self, msg):
     self.state_lock.acquire()
-    pprint(msg)
-    pose = np.array([msg.pose.position.x, msg.pose.position.y, Utils.quaternion_to_angle(msg.pose.Quaternion)])
+    pose = np.array([msg.pose.pose.position.x, msg.pose.pose.position.y, Utils.quaternion_to_angle(msg.pose.pose.orientation)])
 
     if isinstance(self.last_pose, np.ndarray):
       old_control = pose - self.last_pose
       delta_x = old_control[0] * math.cos(self.last_pose[2]) + old_control[1] * math.sin(self.last_pose[2])
       delta_y = -old_control[0] * math.sin(self.last_pose[2]) + old_control[1] * math.cos(self.last_pose[2])
       control = np.array([delta_x, delta_y, old_control[2]])
-      pprint(control)
+
+      # TODO: This just needs an extension.
+      # rot = np.array([[np.cos(self.last_pose[2]), np.sin(self.last_pose[2])], [-np.sin(self.last_pose[2]), np.cos(self.last_pose[2])]])
+      # test_control = np.matmul(rot, old_control[:2])
+      #
+      # pprint(control)
+      # pprint(test_control)
+
+      #assert np.all(control == test_control)
+
+      # pprint(control)
       # Compute the control from the msg and last_pose
       # YOUR CODE HERE
       # Compute control here
@@ -48,7 +62,19 @@ class OdometryMotionModel:
     # Update the proposal distribution by applying the control to each particle
     # Tim: Add noise here
     # YOUR CODE HERE
-    pass
+
+    # TODO josephz: This needs the decoupling trick thingy
+    # rot = np.array([[np.cos(self.particles[:][2]), np.sin(self.particles[:][2])],
+    #            -np.sin(self.particles[:][2]), np.cos(self.particles[:][2])])
+    # np.matmul(rot, self.particles[:, :2])
+
+    noisy_control = control + np.random.normal(loc=ODOM_NOISE_MEAN, scale=ODOM_NOISE_STD, size=self.particles.shape)
+
+    # TODO josephz: This can be faster as well... by doing a vector add.
+    self.particles[:, 0] += np.cos(self.particles[:, 2]) * noisy_control[:, 0] + np.sin(self.particles[:, 2]) * noisy_control[:, 1]
+    self.particles[:, 1] += -np.sin(self.particles[:, 2]) * noisy_control[:, 0] + np.cos(self.particles[:, 2]) * noisy_control[:, 1]
+    self.particles[:, 2] += noisy_control[:, 2]
+    pprint(self.particles)
     
 class KinematicMotionModel:
 
@@ -76,32 +102,59 @@ class KinematicMotionModel:
 
   def motion_cb(self, msg):
     self.state_lock.acquire()
+    print "motion cb"
     
     if self.last_servo_cmd is None:
       return
-
+    print 'a'
     if self.last_vesc_stamp is None:
       self.last_vesc_stamp = msg.header.stamp
-      return
+      # return
+    print 'b'
 
 
     # Convert raw msgs to controls
     # Note that control = (raw_msg_val - offset_param) / gain_param < This converts from raw motor speed to car velocity
     # Tim: Also use the same formula above to the steering angle
-
-    curr_speed = (msg.speed - self.SPEED_TO_ERPM_OFFSET) / self.SPEED_TO_ERPM_GAIN
+    pprint(msg)
+    curr_speed = (msg.state.speed - self.SPEED_TO_ERPM_OFFSET) / self.SPEED_TO_ERPM_GAIN
     curr_steering_angle = (self.last_servo_cmd - self.STEERING_TO_SERVO_OFFSET) / self.STEERING_TO_SERVO_GAIN
-    dt = msg.header.stamp - self.last_vesc_stamp
+    # TODO josephz: what the fuck does this guy do?
+    # dt = msg.header.stamp - self.last_vesc_stamp
 
-    self.apply_motion_model(proposal_dist=self.particles, control=[curr_speed, curr_steering_angle, dt])
+    self.apply_motion_model(proposal_dist=self.particles, control=(curr_speed, curr_steering_angle))
     self.last_vesc_stamp = msg.header.stamp
     self.state_lock.release()
     
   def apply_motion_model(self, proposal_dist, control):
     # Update the proposal distribution by applying the control to each particle
     # YOUR CODE HERE
-    pass
-    
-if __name__ == '__main__':
-  pass
-    
+
+    v, delta = control
+    beta = np.arctan(np.tan(delta/ 2))
+
+    # Kinetic to Odom conversion to remain consistent with particle update.
+    odom_control = np.array([
+      v * np.cos(self.particles[:, 2]), # t - 1
+      v * np.sin(self.particles[:, 2]),
+      v / CAR_LEN * np.sin(2 * beta)       # time t ->
+    ])
+    print control
+    print odom_control
+
+    # Compute particle updates.
+    # TODO josephz: This can be faster as well... by doing a vector add.
+    noiasdf = np.random.normal(loc=ODOM_NOISE_MEAN, scale=ODOM_NOISE_STD, size=self.particles.shape)
+    noisy_control = odom_control + noiasdf
+    a = np.cos(self.particles[:, 2]) * noisy_control[:, 0]
+    b = np.sin(self.particles[:, 2]) * noisy_control[:, 1]
+
+    print a
+    print b
+    self.particles[:, 0] += a + b
+
+    self.particles[:, 1] += -np.sin(self.particles[:, 2]) * noisy_control[:, 0] \
+                            + np.cos(self.particles[:, 2]) * noisy_control[:, 1]
+    self.particles[:, 2] += noisy_control[:, 2]
+    pprint(self.particles)
+
--- src/lab1/src/utils.py
+++ src/lab1/src/utils.py
@@ -3,6 +3,7 @@
 import rospy
 import numpy as np
 import tf.transformations
+from pprint import pprint
 
 # THESE FUNCTIONS MAY OR MAY NOT BE HELPFUL IN YOUR IMPLEMENTATION
 # IMPLEMENT/USE AS YOU PLEASE
@@ -12,7 +13,8 @@ def angle_to_quaternion(angle):
   return quat
 
 def quaternion_to_angle(q):
-  euler = tf.transformations.euler_from_quaterion(g)
+  euler = tf.transformations.euler_from_quaternion([q.x, q.y, q.z, q.w])
+  # pprint(euler)
   return euler[2]
 
 def map_to_world(poses,map_info):
