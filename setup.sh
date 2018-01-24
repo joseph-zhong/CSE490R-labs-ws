ae122d39f4f242736656e9b7b6bcf1bf35c8f068
Joseph Zhong <josephz@cs.washington.edu>
2018-01-24 13:57:58 -0800

    add rangelibc to setup

--- setup.sh
+++ setup.sh
@@ -5,13 +5,19 @@
 #   This helps setup the development environment for the labs.
 #
 
-echo "Installing vesc..."
+echo ""
+echo " -- Installing vesc..."
+echo ""
 sudo apt install ros-kinetic-serial
 
-echo "Installing map-server..."
+echo ""
+echo " -- Installing map-server..."
+echo ""
 sudo apt install ros-kinetic-map-server
 
-echo "Building packages in the standard catkin_ws"
+echo ""
+echo " -- Building packages in the standard catkin_ws"
+echo ""
 if [ ! -d "~/catkin_ws" ]; then
   mkdir -p ~/catkin_ws
   if [ ! -d "~/catkin_ws/racecar_base" ]; then
@@ -19,12 +25,35 @@ if [ ! -d "~/catkin_ws" ]; then
   fi
 fi
 
-echo "Updating packages from racecar_base..."
+echo ""
+echo " -- Updating packages from racecar_base..."
+echo ""
 cd ~/catkin_ws/racecar_base/
 git pull
 cp -r ~/catkin_ws/racecar_base/* ~/catkin_ws/src/
-cd ~/catkin_ws/src/; catkin_make
+cd ~/catkin_ws/; catkin_make
 
+echo ""
+echo " -- Done updating packages from racecar_base..."
+echo ""
+
+echo ""
+echo " -- Installing range_libc"
+echo "    See https://github.com/kctess5/range_libc#python-wrappers "
+echo ""
+pip install --user Cython
+
+cd
+git clone https://github.com/kctess5/range_libc
+cd range_libc/pywrapper
+sudo python setup.py install
+python test.py
+
+echo ""
+echo " -- Done installing range_libc"
+echo ""
+
+echo ""
 echo "Make sure to run the following, and add them to your ~/.bashrc"
 echo
 echo "
