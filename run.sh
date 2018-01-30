6238d43aa83c3548c8a0ad708f29059d9c73dca9
Tim Adamson <tda5@uw.edu>
2018-01-30 14:38:14 -0800

    reverted last commit with correct sensormodel behavior

--- run.sh
+++ run.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-roslaunch lab1 ParticleFilter.launch
+echo $1
+echo $2
+
+cmd="roslaunch lab1 ParticleFilter.launch \
+  max_particles:=$1 \
+  resample_type:=$2"
+
+echo "Running $cmd"
+$cmd
