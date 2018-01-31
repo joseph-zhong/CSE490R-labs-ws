2e2873cc75348146b8273c0907f717ff6fca0b47
Tim Adamson <tda5@uw.edu>
2018-01-31 07:48:02 -0800

    fixed the weights and added submission files

---
+++ submission/MotionModel.py
@@ -0,0 +1,158 @@
+#!/usr/bin/env python
+
+import rospy
+import numpy as np
+import utils as Utils
+import math
+from std_msgs.msg import Float64
+from threading import Lock
+
+from pprint import pprint
+
+# Delete this later
+from nav_msgs.msg import Odometry
+
+# Motion Model Hyperparameters.
+ODOM_NOISE_MEAN = 0.0
+ODOM_NOISE_STD = 2e-2
+
+KINEMATIC_NOISE_MEAN = 0.0
+KINEMATIC_NOISE_STD = 1e-3
+
+
+# Car globals.
+CAR_LEN = 0.33
+
+class OdometryMotionModel:
+
+  def __init__(self, particles, state_lock=None):
+    self.last_pose = None # The last pose that was received
+    self.particles = particles
+    if state_lock is None:
+      self.state_lock = Lock()
+    else:
+      self.state_lock = state_lock
+    
+  def motion_cb(self, msg):
+    self.state_lock.acquire()
+    pose = np.array([msg.pose.pose.position.x, msg.pose.pose.position.y, Utils.quaternion_to_angle(msg.pose.pose.orientation)])
+
+    def rotation_matrix(theta):
+        cos_theta = np.cos(theta)
+        sin_theta = np.sin(theta)
+        return np.array([[cos_theta, sin_theta], [-sin_theta, cos_theta]])
+
+    if isinstance(self.last_pose, np.ndarray):
+      old_control = pose - self.last_pose
+
+      x_prime, y_prime, theta_prime = old_control
+      theta = self.last_pose[2]
+      delta_x, delta_y = rotation_matrix(theta).dot(np.array([x_prime, y_prime]))
+
+      control = np.array([delta_x, delta_y, theta_prime])
+
+      # Compute the control from the msg and last_pose
+      # YOUR CODE HERE
+      # Compute control here
+      
+      self.apply_motion_model(self.particles, control)
+
+    self.last_pose = pose
+    self.state_lock.release()
+    
+  def apply_motion_model(self, proposal_dist, control):
+    # Update the proposal distribution by applying the control to each particle
+    # Tim: Add noise here
+    # YOUR CODE HERE
+
+    noisy_control = control + np.random.normal(loc=ODOM_NOISE_MEAN, scale=ODOM_NOISE_STD, size=self.particles.shape)
+    delta_x = np.cos(self.particles[:, 2]) * noisy_control[:, 0] + -np.sin(self.particles[:, 2]) * noisy_control[:, 1]
+    delta_y = np.sin(self.particles[:, 2]) * noisy_control[:, 0] + np.cos(self.particles[:, 2]) * noisy_control[:, 1]
+    # pprint("Delta_y")
+    # pprint(delta_y)
+
+    self.particles[:, 0] += delta_x
+    self.particles[:, 1] += delta_y
+    self.particles[:, 2] += noisy_control[:, 2]
+    self.particles[:, 2] %= (2 * np.pi)
+    #pprint(self.particles)
+    
+class KinematicMotionModel:
+
+  def __init__(self, particles, state_lock=None):
+    self.last_servo_cmd = None # The most recent servo command
+    self.last_vesc_stamp = None # The time stamp from the previous vesc state msg
+    self.particles = particles
+    self.SPEED_TO_ERPM_OFFSET = float(rospy.get_param("/vesc/speed_to_erpm_offset")) # Offset conversion param from rpm to speed
+    self.SPEED_TO_ERPM_GAIN = float(rospy.get_param("/vesc/speed_to_erpm_gain"))   # Gain conversion param from rpm to speed
+    self.STEERING_TO_SERVO_OFFSET = float(rospy.get_param("/vesc/steering_angle_to_servo_offset")) # Offset conversion param from servo position to steering angle
+    self.STEERING_TO_SERVO_GAIN = float(rospy.get_param("/vesc/steering_angle_to_servo_gain")) # Gain conversion param from servo position to steering angle
+
+    if state_lock is None:
+      self.state_lock = Lock()
+    else:
+      self.state_lock = state_lock
+      
+    # This subscriber just caches the most recent servo position command
+    self.servo_pos_sub = rospy.Subscriber(rospy.get_param("~servo_pos_topic", "/vesc/sensors/servo_position_command"), Float64,
+                                       self.servo_cb, queue_size=1) # Tim: This tells you the position of the servo controlling the steering angle
+                                       
+
+  def servo_cb(self, msg):
+    self.last_servo_cmd = msg.data # Just update servo command
+
+  def motion_cb(self, msg):
+    """
+    Compute controls, with respect to dt
+    :param msg:
+    :return:
+    """
+    #print "motion_cb"
+    self.state_lock.acquire()
+
+    if self.last_servo_cmd is None:
+      self.state_lock.release()
+      return
+    if self.last_vesc_stamp is None:
+      self.last_vesc_stamp = msg.header.stamp
+      # return
+
+
+    # Convert raw msgs to controls
+    # Note that control = (raw_msg_val - offset_param) / gain_param < This converts from raw motor speed to car velocity
+    # Tim: Also use the same formula above to the steering angle
+    # pprint(msg)
+    curr_speed = (msg.state.speed - self.SPEED_TO_ERPM_OFFSET) / self.SPEED_TO_ERPM_GAIN
+    curr_steering_angle = (self.last_servo_cmd - self.STEERING_TO_SERVO_OFFSET) / self.STEERING_TO_SERVO_GAIN
+    dt = msg.header.stamp - self.last_vesc_stamp
+
+    self.apply_motion_model(proposal_dist=self.particles, control=(curr_speed, curr_steering_angle, dt))
+    self.last_vesc_stamp = msg.header.stamp
+    self.state_lock.release()
+    
+  def apply_motion_model(self, proposal_dist, control):
+    #print "applying motion model"
+    # Update the proposal distribution by applying the control to each particle
+    # YOUR CODE HERE
+
+    # todo: review convert dt to value?
+    v, delta, dt = control
+    dt = dt.to_sec()
+
+    noise = np.random.normal(loc=ODOM_NOISE_MEAN, scale=ODOM_NOISE_STD, size=(len(self.particles), 2))
+    noisy_v = v + noise[:, 0]
+    noisy_delta = delta + noise[:, 1]
+
+    theta = self.particles[:, 2]
+
+    delta_theta = noisy_v / CAR_LEN * np.sin(noisy_delta) * dt
+    delta_x = CAR_LEN / np.sin(noisy_delta) * (np.sin(theta + delta_theta) - np.sin(theta))
+    delta_y = CAR_LEN / np.sin(noisy_delta) * (np.cos(theta) - np.cos(theta + delta_theta))
+
+    self.particles[:, 0] += delta_x
+    self.particles[:, 1] += delta_y
+    self.particles[:, 2] += delta_theta
+    self.particles[:, 2] %= (2 * np.pi)
+    #pprint(self.particles)
+
+    # pprint(self.particles)
---
+++ submission/SensorModel.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python
+
+import numpy as np
+import rospy
+import range_libc
+import time
+from threading import Lock
+from sensor_msgs.msg import LaserScan
+
+THETA_DISCRETIZATION = 112  # Discretization of scanning angle
+INV_SQUASH_FACTOR = 0.2  # Factor for helping the weight distribution to be less peaked
+
+Z_SHORT = 0.1  # Weight for short reading
+Z_MAX = 0.015  # Weight for max reading
+Z_RAND = 0.02  # Weight for random reading
+LAMBDA_SHORT = 0.2  # Parameter for short distribution
+SIGMA_HIT = 5.0  # Noise value for hit reading
+Z_HIT = 0.865  # Weight for hit reading
+
+# Z_SHORT = 0.1  # Weight for short reading
+# Z_MAX = 0.005    # Weight for max reading
+# Z_RAND = 0.005   # Weight for random reading
+# LAMBDA_SHORT = 0.2  # Parameter for short distribution
+# SIGMA_HIT = 4.0  # Noise value for hit reading
+# Z_HIT = 0.8    # Weight for hit reading
+
+class SensorModel:
+  def __init__(self, map_msg, particles, weights, state_lock=None):
+    if state_lock is None:
+      self.state_lock = Lock()
+    else:
+      self.state_lock = state_lock
+
+    self.particles = particles
+    self.weights = weights
+
+    self.last_laser = LaserScan()
+
+    self.LASER_RAY_STEP = int(rospy.get_param("~laser_ray_step"))  # Step for downsampling laser scans
+    self.MAX_RANGE_METERS = float(rospy.get_param("~max_range_meters"))  # The max range of the laser
+
+    oMap = range_libc.PyOMap(map_msg)  # A version of the map that range_libc can understand
+    max_range_px = int(self.MAX_RANGE_METERS / map_msg.info.resolution)  # The max range in pixels of the laser
+    self.range_method = range_libc.PyCDDTCast(oMap, max_range_px,
+      THETA_DISCRETIZATION)  # The range method that will be used for ray casting
+    self.range_method.set_sensor_model(
+      self.precompute_sensor_model(max_range_px))  # Load the sensor model expressed as a table
+    self.queries = None
+    self.ranges = None
+    self.laser_angles = None  # The angles of each ray
+    self.downsampled_angles = None  # The angles of the downsampled rays
+    self.do_resample = False  # Set so that outside code can know that it's time to resample
+
+  def lidar_cb(self, msg):
+    self.state_lock.acquire()
+
+    # Compute the observation
+    # obs is a a two element tuple
+    # obs[0] is the downsampled ranges
+    # obs[1] is the downsampled angles
+    # Each element of obs must be a numpy array of type np.float32 (this is a requirement for GPU processing)
+    # Use self.LASER_RAY_STEP as the downsampling step
+    # Keep efficiency in mind, including by caching certain things that won't change across future iterations of this
+    #  callback
+
+    # YOUR CODE HERE
+    # Tam - Downsampled angles shouldn't change.
+    if self.downsampled_angles is None:
+      self.downsampled_angles = np.arange(msg.angle_min, msg.angle_max, msg.angle_increment * self.LASER_RAY_STEP,
+        dtype=np.float32)
+
+    downsampled_ranges = np.array(msg.ranges[::self.LASER_RAY_STEP], dtype=np.float32)
+    isNan = np.isnan(downsampled_ranges)
+    downsampled_ranges[isNan] = msg.range_max
+    obs = (downsampled_ranges, self.downsampled_angles)
+
+    self.apply_sensor_model(self.particles, obs, self.weights)
+    self.weights /= np.sum(self.weights)
+
+    self.last_laser = msg
+    self.do_resample = True
+
+    self.state_lock.release()
+
+  def precompute_sensor_model(self, max_range_px):
+    """
+    Returns a table with columns as expectation while rows as observation measurement
+    :param max_range_px:
+    :return:
+    """
+
+    table_width = int(max_range_px) + 1
+    sensor_model_table = np.zeros((table_width, table_width))
+
+    # Populate sensor model table as specified
+    # Note that the row corresponds to the observed measurement and the column corresponds to the expected measurement
+    # YOUR CODE HERE
+
+    # Populate the matrix
+    def interpolated_pdf(observed, expected):
+      # Sample from normal pdf
+      #      p_hit = norm.pdf(observed, expected, SIGMA_HIT)
+      sigma_squared = SIGMA_HIT ** 2
+      p_hit = (1 / np.sqrt(2 * np.pi * sigma_squared)) * np.exp(
+        -(np.power(observed - expected, 2) / (2 * sigma_squared)))
+
+      condition = observed <= expected
+      p_short = condition * LAMBDA_SHORT * np.exp(-LAMBDA_SHORT * observed)
+
+      # Uniformly distributed
+      p_rand = 1.0 / max_range_px
+
+      # p_max is 1 only if observed = z_max, 0 otherwise
+      p_max = observed == max_range_px
+
+      return Z_HIT * p_hit + Z_SHORT * p_short + Z_MAX * p_max + Z_RAND * p_rand
+
+    # Calculate each entry for the table and normalize by columns.
+    sensor_model_table = np.fromfunction(interpolated_pdf, (table_width, table_width), dtype=np.float32)
+    column_sums = sensor_model_table.sum(axis=0)
+    sensor_model_table /= column_sums
+
+    return sensor_model_table
+
+  def apply_sensor_model(self, proposal_dist, obs, weights):
+
+    obs_ranges = obs[0]
+    obs_angles = obs[1]
+    num_rays = obs_angles.shape[0]
+
+    # Only allocate buffers once to avoid slowness
+    if not isinstance(self.queries, np.ndarray):
+      self.queries = np.zeros((proposal_dist.shape[0], 3), dtype=np.float32)
+      self.ranges = np.zeros(num_rays * proposal_dist.shape[0], dtype=np.float32)
+
+    self.queries[:, :] = proposal_dist[:, :]
+
+    self.range_method.calc_range_repeat_angles(self.queries, obs_angles, self.ranges)
+
+    # Evaluate the sensor model on the GPU
+    self.range_method.eval_sensor_model(obs_ranges, self.ranges, weights, num_rays, proposal_dist.shape[0])
+
+    np.power(weights, INV_SQUASH_FACTOR, weights)
+
+
+if __name__ == '__main__':
+  pass
