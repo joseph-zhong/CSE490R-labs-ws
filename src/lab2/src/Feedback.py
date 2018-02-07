82a74934a9f6fe7f0a11ccd715e4d8f462fc876f
Tim Adamson <tda5@uw.edu>
2018-02-07 12:47:12 -0800

    implemented PID controller

--- src/lab2/src/Feedback.py
+++ src/lab2/src/Feedback.py
@@ -4,10 +4,15 @@ from pprint import pprint
 from ackermann_msgs.msg import AckermannDriveStamped
 
 SPEED = 0.5
+KP = 1
+KI = 1
+KD = 0
 
 class FeedbackController(object):
     def __init__(self, conrol_pub):
         self.control_pub = conrol_pub
+        self.total_error = 0
+        self.last_error = 0
 
     def image_cb(self, msg):
         error = self.img_to_error(msg)
@@ -18,7 +23,11 @@ class FeedbackController(object):
         return 0
 
     def error_to_control(self, error):
-        return 0
+        delta_error = error - self.last_error
+        steering_angle = (KP * error) + (KI * self.total_error) + (KD * delta_error)
+        self.last_error = error
+        self.total_error += error
+        return steering_angle
 
     def publish_controls(self, steering_angle):
         print("moving")
