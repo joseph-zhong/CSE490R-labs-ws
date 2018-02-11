9277ed37f8cb0bd5a6f10872368d57df1c64670e
Joseph Zhong <josephz@cs.washington.edu>
2018-02-10 18:54:48 -0800

    fix controller with visualization change and k callback

--- src/lab2/src/Controller.py
+++ src/lab2/src/Controller.py
@@ -14,33 +14,35 @@ FORWARD_CONTROLLER = "forward"
 
 PUB_CTRL_TOPIC = '/vesc/high_level/ackermann_cmd_mux/input/nav_0'
 PUB_IMG_TOPIC = '/mask_img'
+PUB_IMG_ROI_TOPIC = '/roi_img'
+PUB_IMG_TEMPLATE_TOPIC = '/template_img'
 
-SUB_TOPIC_IMAGE = '/camera/color/image_raw'
+SUB_TOPIC = '/camera/color/image_raw'
 SUB_TOPIC_K = '/camera/color/camera_info'
 
 class Controller(object):
   def __init__(self):
     # Publishes the expected pose
-    print("Creating controller")
     control_pub = rospy.Publisher(PUB_CTRL_TOPIC, AckermannDriveStamped, queue_size=1)
     image_pub = rospy.Publisher(PUB_IMG_TOPIC, Image, queue_size=1)
+    roi_pub = rospy.Publisher(PUB_IMG_ROI_TOPIC, Image, queue_size=1)
+    template_pub = rospy.Publisher(PUB_IMG_TEMPLATE_TOPIC, Image, queue_size=1)
 
     self.CONTROLLER_TYPE = rospy.get_param("~controller_type", FORWARD_CONTROLLER)
     if self.CONTROLLER_TYPE == FORWARD_CONTROLLER:
-      self.controller = ForwardController(control_pub)
+      self.controller = ForwardController(control_pub, template_pub, roi_pub)
       self.k_sub = rospy.Subscriber(SUB_TOPIC_K, CameraInfo, self.controller.k_cb, queue_size=1)
     elif self.CONTROLLER_TYPE == FEEDBACK_CONTROLLER:
-      self.controller = FeedbackController(control_pub, image_pub)
+      self.controller = FeedbackController(control_pub, image_pub, roi_pub)
     else:
       assert False, "Unrecognized controller type: '{}'".format(self.CONTROLLER_TYPE)
 
     # Sets up the subscribers and publishers.
-    self.image_sub = rospy.Subscriber(SUB_TOPIC_IMAGE, Image, self.controller.image_cb, queue_size=1)
+    self.image_sub = rospy.Subscriber(SUB_TOPIC, Image, self.controller.image_cb, queue_size=1)
 
 
 if __name__ == '__main__':
   rospy.init_node('controller', anonymous=True)
-  print('Node created')
   controller = Controller()
 
   while not rospy.is_shutdown():
