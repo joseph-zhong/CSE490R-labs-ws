3c818635679958660da461e8537035ad1b6ae668
Tim Adamson <tda5@uw.edu>
2018-02-12 13:53:44 -0800

    got pid working

--- src/lab2/src/Feedback.py
+++ src/lab2/src/Feedback.py
@@ -8,7 +8,7 @@ from pprint import pprint
 from cv_bridge import CvBridge, CvBridgeError
 from ackermann_msgs.msg import AckermannDriveStamped
 import rospy
-from util import _mask_img, _getDefaultBlobParams, DEFAULT_BLOB_PARAMS
+from util import _mask_img, _getDefaultBlobParams_blue, DEFAULT_BLOB_PARAMS
 
 # Setup Globals.
 SPEED = 0.25
@@ -18,16 +18,17 @@ KD = 0
 MAX_ANGLE = 0.34
 CENTER_OFFSET = 268
 
-ROI_HEIGHT_HI = 200
-ROI_HEIGHT_LO = 10
+#ROI_HEIGHT_HI = 600
+#ROI_HEIGHT_LO = 400
+ROI_THICKNESS = 150
 # HSV triplet boundaries.
 # REVIEW josephz: This needs to be tuned, consider instantiating outside this class.
-boundaries = [
-  ((5, 100, 100), (35, 240, 220))
+blue_boundaries = [
+  ((22, 180, 150), (30, 255, 200))
 ]
 
 class FeedbackController(object):
-  def __init__(self, conrol_pub, image_pub, roi_pub=None, params=_getDefaultBlobParams()):
+  def __init__(self, conrol_pub, image_pub, roi_pub=None, params=_getDefaultBlobParams_blue()):
     self.image_pub = image_pub
     self.control_pub = conrol_pub
     self.total_error = 0
@@ -59,26 +60,28 @@ class FeedbackController(object):
     steering_angle = self.error_to_control(error)
     # process_time = rospy.Time.now().nsecs - start.nsecs
     process_time = time.time() - s_time
-    self.visualize(steering_angle, process_time, image=image, roi_img=roi_img, y=y, error=error)
+    #self.visualize(steering_angle, process_time, image=image, roi_img=roi_img, y=y, error=error)
     self.publish_controls(steering_angle)
-    print
-    print "-----------"
-    print "TOTAL PIPELINE TIME: ", time.time() - s_time
-    print "-----------"
-    print
+    # print
+    # print "-----------"
+    # print "TOTAL PIPELINE TIME: ", time.time() - s_time
+    # print "-----------"
+    # print
 
   def img_to_error(self, msg):
     brg_img = self.cvBridge.imgmsg_to_cv2(msg, desired_encoding="passthrough")
     hsv_img = cv2.cvtColor(brg_img, cv2.COLOR_BGR2HSV)
-    mask_img = _mask_img(hsv_img, boundaries)
+    mask_img = _mask_img(hsv_img, blue_boundaries)
 
+    print "The shape of the mask image is", mask_img.shape
     img_height, img_width, _ = mask_img.shape
+    print "The image height is ", img_height, "and the image width is ", img_width
     if self.img_width is None:
       self.img_width = img_width
 
     # create the cropped image
-    roi_lo = ROI_HEIGHT_LO
-    roi_hi = min(img_height, ROI_HEIGHT_HI)
+    roi_hi = img_height
+    roi_lo = img_height - ROI_THICKNESS
     roi_img = mask_img[roi_lo:roi_hi, :, :]
     src = np.where(roi_img != 0)
     if len(src) == 0 or len(src[0]) == 0:
@@ -91,7 +94,8 @@ class FeedbackController(object):
     # %20pt
     # In particular, here we are only using xy-coordinate pairs from `pt`,
     # but we could also take into account `size, and especially `angle` and `response`.
-    keypoints = self.blobDetector.detect(mask_img)
+    keypoints = self.blobDetector.detect(roi_img)
+    self.visualize_key_points(roi_img, keypoints)
 
     print "len(keypoints): '{}'".format(len(keypoints))
     max_keypoint = None
@@ -125,7 +129,7 @@ class FeedbackController(object):
     ads.drive.speed = SPEED
     self.control_pub.publish(ads)
 
-  def visualize(self, steering_angle=None, process_time=None, image=None, roi_img=None, y=0, error=0):
+  def visualize(self, steering_angle=None, process_time=None, image=None, roi_img=None, y=None, error=0):
     if image is not None:
       if steering_angle is not None:
         cv2.putText(image, "Steering Angle: '{}'".format(steering_angle),
@@ -134,6 +138,8 @@ class FeedbackController(object):
         cv2.putText(image, "Processing Time: '{}'".format(process_time),
             (230, 150), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2, cv2.LINE_AA)
       if y is not None:
+        error = int(error)
+        y = int(y)
         cv2.circle(image,(error, y), 15, (0, 255, 0), 3, cv2.LINE_AA)
       rosImg = self.cvBridge.cv2_to_imgmsg(image)
       self.image_pub.publish(rosImg)
@@ -147,5 +153,13 @@ class FeedbackController(object):
             (230, 150), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2, cv2.LINE_AA)
       ros_roi_img = self.cvBridge.cv2_to_imgmsg(roi_img)
       # self.image_pub.publish(ros_roi_img)
+
+  def visualize_key_points(self, img, key_points):
+    for key_point in key_points:
+      x = int(key_point.pt[0])
+      y = int(key_point.pt[1])
+      cv2.circle(img, (x, y), 15, (255, 0, 0), 3, cv2.LINE_AA)
+    rosImg = self.cvBridge.cv2_to_imgmsg(img)
+    self.image_pub.publish(rosImg)
 
 
