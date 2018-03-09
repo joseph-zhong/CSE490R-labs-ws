408f69705d6e56c3e21dc526b39948fa4dc15f58
Tam Dang <tamdangnadmat@gmail.com>
2018-03-09 14:35:43 -0800

    fill-in-the-blank config

---
+++ src/lab4/src/Dubins.py
@@ -0,0 +1,353 @@
+import math
+import IPython
+import numpy as np
+import matplotlib.pyplot as plt
+
+show_animation = True
+
+def mod2pi(theta):
+  return theta - 2.0 * math.pi * math.floor(theta / 2.0 / math.pi)
+
+
+def pi_2_pi(angle):
+  while(angle >= math.pi):
+    angle = angle - 2.0 * math.pi
+
+  while(angle <= -math.pi):
+    angle = angle + 2.0 * math.pi
+
+  return angle
+
+def LSL(alpha, beta, d):
+  sa = math.sin(alpha)
+  sb = math.sin(beta)
+  ca = math.cos(alpha)
+  cb = math.cos(beta)
+  c_ab = math.cos(alpha - beta)
+
+  tmp0 = d + sa - sb
+
+  mode = ["L", "S", "L"]
+  p_squared = 2 + (d * d) - (2 * c_ab) + (2 * d * (sa - sb))
+  if p_squared < 0:
+    return None, None, None, mode
+  tmp1 = math.atan2((cb - ca), tmp0)
+  t = mod2pi(-alpha + tmp1)
+  p = math.sqrt(p_squared)
+  q = mod2pi(beta - tmp1)
+
+  return t, p, q, mode
+
+def RSR(alpha, beta, d):
+  sa = math.sin(alpha)
+  sb = math.sin(beta)
+  ca = math.cos(alpha)
+  cb = math.cos(beta)
+  c_ab = math.cos(alpha - beta)
+
+  tmp0 = d - sa + sb
+  mode = ["R", "S", "R"]
+  p_squared = 2 + (d * d) - (2 * c_ab) + (2 * d * (sb - sa))
+  if p_squared < 0:
+    return None, None, None, mode
+  tmp1 = math.atan2((ca - cb), tmp0)
+  t = mod2pi(alpha - tmp1)
+  p = math.sqrt(p_squared)
+  q = mod2pi(-beta + tmp1)
+
+  return t, p, q, mode
+
+def LSR(alpha, beta, d):
+  sa = math.sin(alpha)
+  sb = math.sin(beta)
+  ca = math.cos(alpha)
+  cb = math.cos(beta)
+  c_ab = math.cos(alpha - beta)
+
+  p_squared = -2 + (d * d) + (2 * c_ab) + (2 * d * (sa + sb))
+  mode = ["L", "S", "R"]
+  if p_squared < 0:
+    return None, None, None, mode
+  p = math.sqrt(p_squared)
+  tmp2 = math.atan2((-ca - cb), (d + sa + sb)) - math.atan2(-2.0, p)
+  t = mod2pi(-alpha + tmp2)
+  q = mod2pi(-mod2pi(beta) + tmp2)
+
+  return t, p, q, mode
+
+def RSL(alpha, beta, d):
+  sa = math.sin(alpha)
+  sb = math.sin(beta)
+  ca = math.cos(alpha)
+  cb = math.cos(beta)
+  c_ab = math.cos(alpha - beta)
+
+  p_squared = (d * d) - 2 + (2 * c_ab) - (2 * d * (sa + sb))
+  mode = ["R", "S", "L"]
+  if p_squared < 0:
+    return None, None, None, mode
+  p = math.sqrt(p_squared)
+  tmp2 = math.atan2((ca + cb), (d - sa - sb)) - math.atan2(2.0, p)
+  t = mod2pi(alpha - tmp2)
+  q = mod2pi(beta - tmp2)
+
+  return t, p, q, mode
+
+def RLR(alpha, beta, d):
+  sa = math.sin(alpha)
+  sb = math.sin(beta)
+  ca = math.cos(alpha)
+  cb = math.cos(beta)
+  c_ab = math.cos(alpha - beta)
+
+  mode = ["R", "L", "R"]
+  tmp_rlr = (6.0 - d * d + 2.0 * c_ab + 2.0 * d * (sa - sb)) / 8.0
+  if abs(tmp_rlr) > 1.0:
+    return None, None, None, mode
+
+  p = mod2pi(2 * math.pi - math.acos(tmp_rlr))
+  t = mod2pi(alpha - math.atan2(ca - cb, d - sa + sb) + mod2pi(p / 2.0))
+  q = mod2pi(alpha - beta - t + mod2pi(p))
+  return t, p, q, mode
+
+# alpha: The opposite of the angle of the end position
+# beta: The difference between desired end yaw and the angle of the end position
+# d: The straight-line-distance/turning-radius   
+def LRL(alpha, beta, d):
+  sa = math.sin(alpha)
+  sb = math.sin(beta)
+  ca = math.cos(alpha)
+  cb = math.cos(beta)
+  c_ab = math.cos(alpha - beta)
+
+  mode = ["L", "R", "L"]
+  tmp_lrl = (6.0 - d * d + 2.0 * c_ab + 2.0 * d * (- sa + sb)) / 8.0
+  if abs(tmp_lrl) > 1:
+    return None, None, None, mode
+  p = mod2pi(2 * math.pi - math.acos(tmp_lrl))
+  t = mod2pi(-alpha - math.atan2(ca - cb, d + sa - sb) + p / 2.0)
+  q = mod2pi(mod2pi(beta) - alpha - t + mod2pi(p))
+
+  return t, p, q, mode
+
+# Don't call this directly, use dubins_path_planning
+# ex: The end x position
+# ey: The end y position
+# eyaw: The end yaw
+# c: curvature
+def dubins_path_planning_from_origin(ex, ey, eyaw, c):
+  dx = ex
+  dy = ey
+  D = math.sqrt(dx ** 2.0 + dy ** 2.0) #The straight line distance that the car must travel
+  d = D * c # Distance/turning_radius
+  theta = mod2pi(math.atan2(dy, dx)) # The yaw of the end position
+  alpha = mod2pi(0.0 - theta) # The opposite of the yaw of the end poistion
+  beta = mod2pi(eyaw - theta) # The difference between the desired ending yaw position and the result of going straight towards it
+
+  best_t, best_p, best_q, best_mode = None, None, None, None
+  best_cost = float("inf")
+
+  #-------------------------------------------------------------------
+  # YOUR CODE HERE
+  #
+  # Loop through all 6 of the planners, asking each one to compute a path
+  #   Each planner will return t,p,q and mode
+  #   t is the (signed) arc length of the first portion of the path, 
+  #   p is (signed) arc length of the second portion, q is (signed) arc length of the third portion
+  #   mode indicates which planner the path came from
+  # Find the planner that returns the path with the smallest total arc length, (abs(t) + abs(p) + abs(q))
+  # Set best_t,best_p,best_q, and best_mode to the t,p,q,and mode returned by the best planner
+  #--------------------------------------------------------------------
+
+  # For each planner, calculate a path and score.
+  planners = [LSL, RSR, LSR, RSL, RLR, LRL]
+  projected_paths = [planner(alpha, beta, d) for planner in planners]
+
+  # Filter out NONE instances
+  def valid_arcs(tqpm):
+    t, q, p, _ = tqpm
+    return not (t is None or q is None or p is None)
+
+  projected_paths = filter(valid_arcs, projected_paths)
+
+  # Score each rollout
+  scores = [abs(t) + abs(p) + abs(q) for (t, p, q, _) in projected_paths]
+
+  # Calculate the best score and retrieve the path that produced it.
+  best_path_idx = np.argmin(scores)  # Smallest total arc length
+  best_t, best_p, best_q, best_mode =  projected_paths[best_path_idx]
+  best_cost = scores[best_path_idx]
+
+  px, py, pyaw = generate_course([best_t, best_p, best_q], best_mode, c) # Turns arc lengths into points along path
+
+  return px, py, pyaw, best_mode, best_cost
+
+def dubins_path_planning(s, e, c):
+  """
+  Dubins path plannner
+
+  input:
+      sx x position of start point [m]
+      sy y position of start point [m]
+      syaw yaw angle of start point [rad]
+      ex x position of end point [m]
+      ey y position of end point [m]
+      eyaw yaw angle of end point [rad]
+      c curvature [1/m]
+
+  output:
+      px
+      py
+      pyaw
+      mode
+
+  """
+  
+  sx, sy, syaw = s[0], s[1], s[2]
+  ex, ey, eyaw = e[0], e[1], e[2]
+
+  # Get path in frame of the source
+  ex = ex - sx
+  ey = ey - sy
+  lex = math.cos(syaw) * ex + math.sin(syaw) * ey # Note that we are effectively rotating by -syaw
+  ley = - math.sin(syaw) * ex + math.cos(syaw) * ey # Note that we are effectively rotating by -syaw
+  leyaw = eyaw - syaw
+
+  # Get the plan (w.r.t the source frame)
+  lpx, lpy, lpyaw, mode, clen = dubins_path_planning_from_origin(lex, ley, leyaw, c)
+
+  # Convert back to world coordinates
+  px = [math.cos(-syaw) * x + math.sin(-syaw) * # Note that we are effectively rotating by syaw
+        y + sx for x, y in zip(lpx, lpy)]
+  py = [- math.sin(-syaw) * x + math.cos(-syaw) * # Note that we are effectively rotating by syaw
+        y + sy for x, y in zip(lpx, lpy)]
+  pyaw = [pi_2_pi(iyaw + syaw) for iyaw in lpyaw]
+
+  # Filter out loops from path
+  ppx, ppy, ppyaw, pclen = process_dubins(sx, sy, syaw, px, py, pyaw, clen)
+
+  return ppx, ppy, ppyaw, pclen
+
+def path_length(s, e, c):
+  px, py, pyaw, cost = dubins_path_planning(s, e, c)
+  return cost
+
+def generate_course(length, mode, c):
+
+  px = [0.0]
+  py = [0.0]
+  pyaw = [0.0]
+
+  for m, l in zip(mode, length):
+    pd = 0.0
+    if m is "S":
+      d = 0.3 * c
+    else:  # turning couse
+      d = math.radians(3.0)
+
+    while pd < abs(l - d):
+      #  print(pd, l)
+      px.append(px[-1] + d / c * math.cos(pyaw[-1]))
+      py.append(py[-1] + d / c * math.sin(pyaw[-1]))
+
+      if m is "L":  # left turn
+        pyaw.append(pyaw[-1] + d)
+      elif m is "S":  # Straight
+        pyaw.append(pyaw[-1])
+      elif m is "R":  # right turn
+        pyaw.append(pyaw[-1] - d)
+      pd += d
+    else:
+      d = l - pd
+      px.append(px[-1] + d / c * math.cos(pyaw[-1]))
+      py.append(py[-1] + d / c * math.sin(pyaw[-1]))
+
+      if m is "L":  # left turn
+        pyaw.append(pyaw[-1] + d)
+      elif m is "S":  # Straight
+        pyaw.append(pyaw[-1])
+      elif m is "R":  # right turn
+        pyaw.append(pyaw[-1] - d)
+      pd += d
+
+  return px, py, pyaw
+
+def plot_arrow(x, y, yaw, length=1.0, width=0.5, fc="r", ec="k"):
+  """
+  Plot arrow
+  """
+
+  if not isinstance(x, float):
+    for (ix, iy, iyaw) in zip(x, y, yaw):
+      plot_arrow(ix, iy, iyaw)
+  else:
+    plt.arrow(x, y, length * math.cos(yaw), length * math.sin(yaw),
+            fc=fc, ec=ec, head_width=width, head_length=width)
+    plt.plot(x, y)
+
+# Filter out loops from path
+def process_dubins(startx, starty, enda, px, py, pa, cost):
+
+  '''
+  Naive processing of the output
+  Ensuring there are no 2pi rotations due to numerical issues
+  '''
+  pcost = cost
+  eps = 1e-6
+  for i in range(1,len(px)-1):
+    check1 = abs(px[i] - startx) < eps
+    check2 = abs(py[i] - starty) < eps
+    check3 = abs(pa[i] - enda) < eps
+    if check1 and check2 and check3:
+      return px[i:], py[i:], pa[i:], cost - math.radians(360.0)
+
+  return px, py, pa, cost
+
+def dubin_rollout(dx, dy, d_theta, test_msg):
+  print test_msg
+  c = 1.0 / model.TURNING_RADIUS
+
+  px, py, pyaw, best_mode, best_cost = dubins_path_planning_from_origin(dx, dy, d_theta, c)
+  print "px", px
+  print "py", py
+  print "pyaw", pyaw
+  print "best_mode", best_mode
+  print "best_cost", best_cost
+  print
+
+def dubin_rollout_integration(s, e, test_msg, i=1):
+  """
+  Performs a calculation of a Dubin's path; prints every 10th configuration.
+  """
+
+  print test_msg
+  c = 1.0 / model.TURNING_RADIUS
+
+  px, py, pyaw, cost = dubins_path_planning(s, e, c)
+  print "px", px[::i]
+  print "py", py[::i]
+  print "pyaw", pyaw[::i]
+  print "cost", cost
+  print
+
+def main():
+  print "Starting Dubins"
+
+  # Write TEST CODE HERE!
+  p0 = (0.0, 0.0, 0.0)
+  p1 = (1.0, 0.0, 0.0)
+  p2 = (0.0, 1.0, 0.0)
+  p3 = (1.0, 1.0, 0.0)
+
+  # Driving straight on x
+  dubin_rollout_integration(p0, p1, "Driving straight on x:")
+
+  # Driving straight on y
+  dubin_rollout_integration(p0, p2, "Driving straight on y:", i=10)
+
+  # Mix of x and y:
+  dubin_rollout_integration(p0, p3, "Mix of X and Y:", i=10)
+
+
+if __name__ == '__main__':
+  main()
---
+++ src/lab4/src/ObstacleManager.py
@@ -0,0 +1,122 @@
+import cv2
+import math
+import numpy
+import IPython
+import Dubins
+import Utils
+import KinematicModel as model
+
+class ObstacleManager(object):
+
+  def __init__(self, mapMsg):
+    # Setup the map
+    self.map_info = mapMsg.info
+    self.mapImageGS = numpy.array(mapMsg.data, dtype=numpy.uint8).reshape((mapMsg.info.height, mapMsg.info.width,1))
+
+    # Retrieve the map dimensions
+    height, width, channels = self.mapImageGS.shape
+    self.mapHeight = height
+    self.mapWidth = width
+    self.mapChannels = channels
+
+    # Binarize the Image
+    self.mapImageBW = 255*numpy.ones_like(self.mapImageGS, dtype=numpy.uint8)
+    self.mapImageBW[self.mapImageGS==0] = 0
+    self.mapImageBW = self.mapImageBW[::-1,:,:] # Need to flip across the y-axis    
+    
+    # Obtain the car length and width in pixels
+    self.robotWidth = int(model.CAR_WIDTH/self.map_info.resolution + 0.5)
+    self.robotLength = int(model.CAR_LENGTH/self.map_info.resolution + 0.5)
+
+  # Check if the passed config is in collision
+  # config: The configuration to check (in meters and radians)
+  # Returns False if in collision, True if not in collision
+  def get_state_validity(self, config):
+
+    # Convert the configuration to map-coordinates -> mapConfig is in pixel-space
+    mapConfig = Utils.world_to_map(config, self.map_info)
+
+    # ---------------------------------------------------------
+    # YOUR CODE HERE
+    #
+    # Return true or false based on whether the configuration is in collision
+    # Use self.robotWidth and self.robotLength to represent the size of the robot
+    # Also return false if the robot is out of bounds of the map
+    # Although our configuration includes rotation, assume that the
+    # rectangle representing the robot is always aligned with the coordinate axes of the
+    # map for simplicity
+    # ----------------------------------------------------------
+
+    assert isinstance(mapConfig, (list, tuple)) and len(mapConfig) == 3
+    x, y, theta = mapConfig
+    halfLen = self.robotLength / 2
+    halfWidth = self.robotWidth / 2
+    # REVIEW josephz: Figure out how to incorporate theta to differentiate the two cases?
+    # Validity for car length along the map length.
+    valid = self.mapImageBW[
+            max(0, y-halfLen):min(self.mapHeight, y+halfLen),
+            max(0, x-halfWidth):min(self.mapWidth, x+halfWidth)]
+
+    # Validity for car length along the map width.
+    valid2 = self.mapImageBW[
+            max(0, y - halfWidth):min(self.mapHeight, y + halfWidth),
+            max(0, x - halfLen):min(self.mapWidth, x + halfLen)]
+    # plt.figure()
+    # valid[:] = 50
+    # valid2[:] = 100
+    # plt.imshow(np.squeeze(self.mapImageBW))
+    # plt.imshow(np.squeeze(a))
+    # plt.imshow(np.squeeze(b))
+    # plt.show()
+    return np.sum(valid) == np.sum(valid2) == 0
+
+  # Check if there is an unobstructed edge between the passed configs
+  # config1, config2: The configurations to check (in meters and radians)
+  # Returns false if obstructed edge, True otherwise
+  def get_edge_validity(self, config1, config2):
+    # -----------------------------------------------------------
+    # YOUR CODE HERE
+    #
+    # Check if endpoints are obstructed, if either is, return false
+    # Find path between two configs using Dubins
+    # Check if all configurations along Dubins path are obstructed 
+    # -----------------------------------------------------------
+
+    # REVIEW josephz: Is endpoint checking incorporated within path?
+    path = Dubins.dubins_path_planning(config1, config2, 1.0 / model.TURNING_RADIUS)
+    plt.show()
+    return all(self.get_state_validity(config) for config in path)
+
+# Test
+if __name__ == '__main__':
+  print "Starting obstacle manager"
+  rospy.init_node("obstacle_manager_test", anonymous=True)
+
+  # Get the map
+  map_service_name = rospy.get_param("~static_map", "static_map")
+  print("Getting map from service: ", map_service_name)
+  rospy.wait_for_service(map_service_name)
+  map_msg = rospy.ServiceProxy(map_service_name, GetMap)().map
+
+  # Create the obstacle manager
+  obs_manager = ObstacleManager(map_msg)
+
+  print "Testing middle of the hall way"
+  print "In hall with skinny triangle: Should be True...", obs_manager.get_state_validity([0.62338411808, -0.209855854511, 0.0])
+  print "Skinny triangle: Should be False...", obs_manager.get_state_validity([-0.226983860135, 1.16524136066, 0.0])
+  print "Next to chair: Should be False...", obs_manager.get_state_validity([2.71258091927, 2.31397628784, 0.0])
+  print "Inside chair: Should be False...", obs_manager.get_state_validity([2.07119321823, 3.0112221241, 0.0])
+
+  print "Testing inside square"
+  print "Upper Left corner inside: Should be False...", obs_manager.get_state_validity([10.3908414841, 14.4619655609, 0.0])
+  print "Bottom Left corner inside: Should be False...", obs_manager.get_state_validity([3.79972362518, 8.73226356506, 0.0])
+  print "Upper Right corner inside: Should be False...", obs_manager.get_state_validity([17.0216236115, 7.27234268188, 0.0])
+  print "Bottom Right corner inside: Should be False...", obs_manager.get_state_validity([10.4382705688, 1.49695897102, 0.0])
+
+  print "Testing middle square"
+  print "Upper Left corner middle: Should be True...", obs_manager.get_state_validity([10.4908685684, 15.4024438858, 0.0])
+  print "Bottom Left corner middle: Should be True...", obs_manager.get_state_validity([3.07843399048, 8.75644397736, 0.0])
+  print "Upper Right corner middle: Should be True...", obs_manager.get_state_validity([17.6345787048, 7.33979320526, 0.0])
+  print "Bottom Right corner middle: Should be True...", obs_manager.get_state_validity([9.97846317291, 0.0722773820162, 0.0])
+  plt.show()
+
